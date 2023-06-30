8d783bb295e781f1dda28e2e90d3a89bb650bf70
florinv23 <100446582+florinv23@users.noreply.github.com>
2023-06-30 22:20:19 +0300

    Add logging filter

---
+++ src/main/java/com/boot/gateway/security/LoggingFilter.java
@@ -0,0 +1,34 @@
+package com.boot.gateway.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
+        Instant start = Instant.now();
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> {
+                    Instant finish = Instant.now();
+                    long time = Duration.between(start, finish).toMillis();
+                    log.info("{}: {}ms", exchange.getRequest().getPath(),  time);
+                }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
