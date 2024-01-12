6c5d1ea209640bc32faccf553a77d4423f6003e1
florinv23 <100446582+florinv23@users.noreply.github.com>
2024-01-12 23:07:09 +0200

    Permit get images

--- src/main/java/com/boot/gateway/security/SecurityTokenConfig.java
+++ src/main/java/com/boot/gateway/security/SecurityTokenConfig.java
@@ -47,7 +47,7 @@ public class SecurityTokenConfig {
 				.authorizeExchange()
 				.pathMatchers(HttpMethod.OPTIONS).permitAll()
 				.pathMatchers(HttpMethod.POST, "/login", "/user/", "/user/*", "/user/customerMessage", "/order").permitAll()
-				.pathMatchers(HttpMethod.GET, "/user/confirm/*", "/product", "/product/*", "/order/*").permitAll()
+				.pathMatchers(HttpMethod.GET, "/user/confirm/*", "/product", "/product/**", "/order/*").permitAll()
 				.pathMatchers(HttpMethod.PUT, "/user/confirm/*", "/user/password/reset/*", "/user/password/change").permitAll()
 				.anyExchange().authenticated()
 				.and().build();
